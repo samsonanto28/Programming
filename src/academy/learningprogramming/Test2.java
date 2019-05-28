9619814d9f5cbcc0b0ebc95cd21e31c704eb53bf
Xcalibur <samsonanto28@gmail.com>
2019-05-28 16:41:22 +0530

    Test2 updated

--- src/academy/learningprogramming/Test2.java
+++ src/academy/learningprogramming/Test2.java
@@ -1,4 +1,13 @@
 package academy.learningprogramming;
 
 public class Test2 {
+
+    Test2(){
+        System.out.println("Constructor");
+    }
+
+    void func1(){
+        System.out.println("Func1...");
+    }
+
 }
