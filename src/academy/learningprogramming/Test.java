8d37f4ce7a01e5531f3d81c1bcdf6d70cd85c92a
Xcalibur <samsonanto28@gmail.com>
2019-05-28 12:02:34 +0530

    Add new class test, add constructor and func1

---
+++ src/academy/learningprogramming/Test.java
@@ -0,0 +1,11 @@
+package academy.learningprogramming;
+
+public class Test {
+    Test() {
+        System.out.println("Constructer Add....");
+    }
+
+    void func1() {
+        System.out.println("Func1 excuting....");
+    }
+}
